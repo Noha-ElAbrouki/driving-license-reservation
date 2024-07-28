6684b8c565828d1a5c572a57a886328eead217d5
nouha <nohaelabrouki@gmail.com>
2024-07-28 16:47:15 +0100

    fixes

--- src/rdvs/search/index.jsx
+++ src/rdvs/search/index.jsx
@@ -2,14 +2,13 @@ import SearchBar from 'material-ui-search-bar';
 import useStyles from '../rdvs.styles';
 
 const Search = ({ value, onCancel, onChange }) => {
-  const { classes } = useStyles();
+  const classes = useStyles();
 
   return (
     <SearchBar
       value={value}
       onChange={onChange}
       onCancelSearch={onCancel}
-      // style={{ width: 200 }}
       className={classes.searchBar}
     />
   );
